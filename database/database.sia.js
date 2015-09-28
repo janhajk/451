8e5b626478dee34c6114a873742deaab38040720
janhajk <janhajk@gmail.com>
2015-09-28 08:43:01 +0000

    database work

--- database/database.sia.js
+++ database/database.sia.js
@@ -1,54 +1,58 @@
 var mongoose = require('mongoose');
 
 var sia451Schema = mongoose.Schema({
-    
-    /*
-    title: { type: String, index: true },
-    search: {
-        titles: {type: [String], index: true},
-        years:  {type: [Number], index: true},
-    },
-    info: {
-        runtime: {type: Number, default: 0},
-        year: { type: Number, index: true },
-        synopsis: {type: String, default: ''},
-        ids: {
-            imdb: String,
-            tomatoes: Number,
-            tmdb: Number,
-        },
-        ratings: {
-            imdb: Number,
-            tomatoes: Number,
-            tmdb: Number,
-        },
-        posters: {
-            thumbnail: String,
-            profile: String,
-            original: String
-        },
-        mpaa_rating: String,
-        critics_consensus: String,
-        lastUpdated: Date
-    },
-    sites: [{
-        link: String,
-        title: String,
-        resolution: String,
-        pubdate: Date,
-        lastRipped: Date
-    }],
-    links: [{
-        link: String,
-        type: String,
-        status: Number,
-        lastChecked: Date
-    }],
-    resolutions: {type: [String]},
-    lastUpdate: {type: Date, index: true},
-    dateAdded: Date,
-    display: {type: Boolean, default:true, index: true}
-    */
+   _id: Number,
+   properties: {
+      'Erstellungsdatum'        : {type: Date,   index:true},
+      'Schnittstellenversion'   : {type: Number, index:false},
+      'Fehlerstufe'             : {type: Number, index:false, default: 0},
+      'Prüfung'                 : {type: Number, index:false},
+      'Bauherrenkurzbezeichnung': {type: String, index:false},
+      'Dokumenten-Urheber'      : {type: String, index:false},
+      'Sprachcode'              : {type: String, index:false},
+      'Stellung'                : {type: String, index:false},
+      'Dokumentencode'          : {type: String, index:false},
+      'Dokuementenstatus'       : {type: String, index:false},
+      'Vergabeeinheit'          : {type: String, index:false},
+      'Hilfsnummerierung'       : {type: String, index:false},
+      'Datenträgernummer'       : {type: String, index:false},
+      'Dokuementenversion'      : {type: String, index:false},
+      'Projektidentifikation'   : {type: String, index:false},
+      'Projektbezeichnung'      : {type: String, index:false},
+      'Dokumentenidentifikation': {type: String, index:false},
+      'Vergabeeinheit'          : {type: String, index:false},
+      'Softwarehersteller'      : {type: String, index:false},
+      'Telefon'                 : {type: String, index:false},
+      'Programmversion'         : {type: String, index:false}
+   },
+   positionen: [{
+      'Recordart'               : {type: String, index: false},
+      'Kapitelnummer'           : {type: Number, index: false},
+      'leer1'                   : {type: String, index: false},
+      'Ausgabejahr'             : {type: Number, index: false},
+      'Leistungsposition'       : {type: Number, index: false},
+      'Variablennummer'         : {type: Number, index: false},
+      'Zeilennummer'            : {type: Number, index: false},
+      'Objektgliederung'        : {type: String, index: false},
+      'Positionslage'           : {type: String, index: false},
+      'Variantengruppe'         : {type: String, index: false},
+      'Variante'                : {type: String, index: false},
+      'Verbandskalkulation'     : {type: String, index: false},
+      'Recordtyp'               : {type: String, index: false},
+      'leer2'                   : {type: String, index: false},
+      'Mengenart'               : {type: String, index: false},
+      'Vorzeichen'              : {type: String, index: false},
+      'Menge'                   : {type: String, index: false},
+      'Mengeneinheit'           : {type: String, index: false},
+      'leer3'                   : {type: String, index: false},
+      'Vorzeichen Preis'        : {type: String, index: false},
+      'Preis'                   : {type: String, index: false},
+      'Laufnummer'              : {type: String, index: false},
+      'KAG-Code'                : {type: String, index: false},
+      'Elementcode'             : {type: String, index: false},
+      'Positionstext'           : {type: String, index: false},
+      'Spezielle Codierungen'   : {type: String, index: false}
+}]
 });
 
 var sia451 = mongoose.model('sia451', sia451Schema);
